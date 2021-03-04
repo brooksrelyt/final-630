2e85483f69f56a64ea133aeb0b58f8f482b5d839
Ty <tdbrooks@umd.edu>
2021-03-04 14:28:59 -0500

    updated code

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
-function createFilter(type) { // Helper function
+function createFilter(type) {
   var filter = [];
-  $("#" + type + '-filter input:checked').each(function(index, element) {
-    filter.push("[data-" + type + "^='" + element.value + "']"); // Use ^= to match the start of the actual value
+  $('#' + type + '-filter input:checked').each(function (index, element) {
+    filter.push('[data-' + type + "^='" + element.value + "']");
   });
-  return filter.join(",") || "*"; // Asterisk means: no filter at all
+  return filter.join(',') || '*';
 }
 
-$(document).on('change', 'input[type=checkbox]', function() {
+$(document).on('change', 'input[type=checkbox]', function () {
   $('.leaflet-zoom-animated > g > circle')
-  .hide()
-  .filter(createFilter("star")) // Any of the checked star selections, AND
-  .filter(createFilter("review")) // Any of the review selections, AND
-  .filter(createFilter("cat")) // Any of the cat selections, AND
-  .filter(createFilter("price")) // Any of the price selections
-  .show(); // Show the matches of the above filter
+    .hide()
+    .filter(createFilter('star'))
+    .filter(createFilter('review'))
+    .filter(createFilter('cat'))
+    .filter(createFilter('price'))
+    .show();
 
-  $('#reset').click(function(e) {
+  $('#reset').click(function (e) {
     $('input:checked').removeAttr('checked');
     $('.leaflet-zoom-animated > g > circle').show();
     e.preventDefault();
   });
 });
-
